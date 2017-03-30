a7f60eea8afa0fb19657aba021d2456e56424d5a
huangpeibin <554980423@qq.com>
2017-03-30 18:58:41 +0800

    hpb

--- client/js/common/global.js
+++ client/js/common/global.js
@@ -1,6 +1,6 @@
 var globalApp = angular.module("globalApp",[]);
 
-globalApp.value("baseUrl","http://10.3.135.48:8080");//10.3.135.48//yhy
+globalApp.value("baseUrl","http://172.17.98.1:8080");//10.3.135.48//yhy
 
 
 globalApp.value("color","#ccc");

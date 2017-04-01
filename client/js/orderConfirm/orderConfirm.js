4722181ecf56390f4339e3f688a141660a27a154
799616945@qq.com <799616945@qq.com>
2017-04-01 15:25:53 +0800

    kam

--- client/js/orderConfirm/orderConfirm.js
+++ client/js/orderConfirm/orderConfirm.js
@@ -23,5 +23,32 @@ confirmApp.directive('content',function(){
 })
 
 confirmApp.controller("confirmCtrl",["$scope","$http","baseUrl",function($scope,$http,baseUrl){
-
+  $scope.time = 60 * 15;
+  $scope.event = {
+    timeout: function(){
+     $scope.time --;
+     var seconds = $scope.time % 60;
+     var minutes = Math.floor($scope.time / 60);
+     $scope.timeCount = minutes +'分' + seconds + '秒';
+       if($scope.time <= 0){
+        $('<div class="mask">\
+            <div class="prompt">\
+              <div class="message">\
+                <h3>温馨提示</h3>\
+                <p>超过支付等待时间，订单已失效，请重新预定</p>\
+             </div>\
+             <a href="">重新预订</a>\
+            </div>\
+          </div>').appendTo('body');
+       }
+    }
+  }
+  if($scope.time > 0 ){
+    var time = setInterval(function(){
+      $("#timeCount").trigger("click")
+      if($scope.time <= 0){
+        clearInterval(time)
+      }
+    },1000)
+  }
 }])
